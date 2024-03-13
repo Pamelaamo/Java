4fa2219388d3790ff8e6a68b607317190b236c81
Pâmela Amorim <pamelaamorim8@hotmail.com>
2024-03-13 20:50:27 -0300

    remanejando mini teste em java de pasta

---
+++ caneta1.java
@@ -0,0 +1,40 @@
+import main2;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Está tampada? " + this.tampada);
+        System.out.println("Carga: " + this.carga);
+    }
+
+    void rabiscar() {
+        if (this.tampada) {
+            System.out.println("Erro! Não é possível rabiscar com a caneta tampada.");
+        } else {
+            System.out.println("Rabiscando...");
+        }
+    }
+
+    void tampar() {
+        this.tampada = true;
+    }
+
+    void destampar() {
+        this.tampada = false;
+    }
+
+    
+}
+
+
+
+
+
---
+++ main2.java
@@ -0,0 +1,17 @@
+import caneta1;
+
+public class main{
+
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.modelo = "Bic";
+        c1.cor = "Vermelha";
+        c1.ponta = 0.8f; // Adicionado sufixo f para indicar float
+        c1.tampada = false;
+
+        c1.status(); // Exibe o status da caneta
+        c1.rabiscar(); // Tenta rabiscar
+        c1.tampar(); // Tampar a caneta
+        c1.rabiscar(); // Tenta rabiscar novamente após tampar
+    }
+}
